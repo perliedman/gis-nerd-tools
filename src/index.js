eac8543b03e43ba16661b6c47f7a11c0f7fd0327
Per Liedman <per@liedman.net>
2013-11-22 12:18:12 +0100

    Added attribution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 var L = require('leaflet'),
-    map = L.map('map'),
+    map = L.map('map', { attributionControl: false }),
     Repository = require('./repository'),
     Sidebar = require('./sidebar.js'),
     Projections = require('./projections'),
@@ -19,7 +19,12 @@ map.on('click', function(e) {
   coordDisplay.show(e.latlng);
 });
 
-L.tileLayer('https://a.tiles.mapbox.com/v3/liedman.map-mmgw7jk5/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('https://a.tiles.mapbox.com/v3/liedman.map-mmgw7jk5/{z}/{x}/{y}.png', {
+    attribution: 'Maps by <a href="https://www.mapbox.com/about/maps/">MapBox</a>'
+}).addTo(map);
+
+L.control.attribution({ position: 'bottomleft' }).addTo(map);
+
 geomLayer.addTo(map);
 map.setView([0, 0], 2);
 
