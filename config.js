9bd8b1840d8c578a73a32819d75f1cb03aa6422b
Per Liedman <per@liedman.net>
2014-10-13 20:58:52 +0200

    Update map id

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
   tiles: {
-    url: 'https://a.tiles.mapbox.com/v3/liedman.h9ekn0f1/{z}/{x}/{y}.png',
+    url: 'https://a.tiles.mapbox.com/v3/liedman.jokgn3nn/{z}/{x}/{y}.png',
     attribution: 'Maps by <a href="https://www.mapbox.com/about/maps/">MapBox</a>'
   }
 };
